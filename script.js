50197acc1d7fc89ac778406f292af024f0c2bb9e
chahin5-hub <sebaachahin3@gmail.com>
2026-04-25 21:55:29 +0200

    Replace goPricing function with filterProjects function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function goPricing() {
-  document.getElementById("pricing").scrollIntoView({
-    behavior: "smooth"
+function filterProjects(type) {
+  let projects = document.querySelectorAll(".project");
+
+  projects.forEach(p => {
+    if (type === "all") {
+      p.style.display = "block";
+    } else if (p.classList.contains(type)) {
+      p.style.display = "block";
+    } else {
+      p.style.display = "none";
+    }
   });
 }
